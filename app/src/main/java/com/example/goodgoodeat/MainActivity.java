1c08406564e98fe1c92a0c2295c2396a4b78c0c0
kkonlygirl <2050058344@qq.com>
2022-11-07 10:48:02 +0800

    test

--- app/src/main/java/com/example/goodgoodeat/MainActivity.java
+++ app/src/main/java/com/example/goodgoodeat/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         System.out.print("vvvv");
         System.out.print("vvwwwv");
+        System.out.print("vvwwwv");
         setContentView(R.layout.activity_main);
     }
 }
