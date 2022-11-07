e25bfd43c97c5f1fcb2773a79f48a1eb2878afb8
kkonlygirl <2050058344@qq.com>
2022-11-07 10:33:54 +0800

    test

--- app/src/main/java/com/example/goodgoodeat/MainActivity.java
+++ app/src/main/java/com/example/goodgoodeat/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.print("vvvv");
+        System.out.print("vvwwwv");
         setContentView(R.layout.activity_main);
     }
 }
